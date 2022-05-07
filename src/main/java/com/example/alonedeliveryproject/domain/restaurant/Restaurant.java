8f19c2b442dd7673b60536c76f948f4b54735bd4
zzangoobrother <com1368@naver.com>
2022-05-07 21:23:52 +0900

    mock 객체로 변경

--- src/main/java/com/example/alonedeliveryproject/domain/restaurant/Restaurant.java
+++ src/main/java/com/example/alonedeliveryproject/domain/restaurant/Restaurant.java
@@ -33,36 +33,4 @@ public class Restaurant {
     this.minOrderPrice = minOrderPrice;
     this.deliveryFee = deliveryFee;
   }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-
-    Restaurant that = (Restaurant) o;
-
-    if (minOrderPrice != that.minOrderPrice) {
-      return false;
-    }
-    if (deliveryFee != that.deliveryFee) {
-      return false;
-    }
-    if (id != null ? !id.equals(that.id) : that.id != null) {
-      return false;
-    }
-    return name != null ? name.equals(that.name) : that.name == null;
-  }
-
-  @Override
-  public int hashCode() {
-    int result = id != null ? id.hashCode() : 0;
-    result = 31 * result + (name != null ? name.hashCode() : 0);
-    result = 31 * result + minOrderPrice;
-    result = 31 * result + deliveryFee;
-    return result;
-  }
 }
--- src/test/java/com/example/alonedeliveryproject/web/restaurant/service/RestaurantServiceTest.java
+++ src/test/java/com/example/alonedeliveryproject/web/restaurant/service/RestaurantServiceTest.java
@@ -1,29 +1,43 @@
 package com.example.alonedeliveryproject.web.restaurant.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import com.example.alonedeliveryproject.domain.restaurant.Repository.RestaurantRepository;
 import com.example.alonedeliveryproject.domain.restaurant.Restaurant;
 import com.example.alonedeliveryproject.web.restaurant.dto.RestaurantSaveDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class RestaurantServiceTest {
 
   @Mock
   private RestaurantRepository restaurantRepository;
 
-  @Autowired
+  @InjectMocks
   private RestaurantService restaurantService;
+
+  private Restaurant restaurant;
 
   private RestaurantSaveDto.Request restaurantSaveRequestDto;
 
   @BeforeEach
   void setup() {
+    restaurant = Restaurant.builder()
+        .name("쉑쉑 강남점")
+        .minOrderPrice(100_000)
+        .deliveryFee(10_000)
+        .build();
+
     restaurantSaveRequestDto = RestaurantSaveDto.Request.builder()
         .name("쉑쉑 강남점")
         .minOrderPrice(100_000)
@@ -33,15 +47,16 @@ class RestaurantServiceTest {
 
   @Test
   void 음식점_등록() {
-    Restaurant saveRestaurant = restaurantService.restaurantSave(restaurantSaveRequestDto);
+    ArgumentCaptor<Restaurant> captor = ArgumentCaptor.forClass(Restaurant.class);
+    given(restaurantRepository.save(any())).willReturn(restaurant);
 
-    RestaurantSaveDto.Request saveRestaurantSaveRequestDto
-        = RestaurantSaveDto.Request.builder()
-        .name("쉑쉑 강남점")
-        .minOrderPrice(100_000)
-        .deliveryFee(10_000)
-        .build();
+    restaurantService.restaurantSave(restaurantSaveRequestDto);
 
-    assertThat(restaurantSaveRequestDto).isEqualTo(saveRestaurantSaveRequestDto);
+    verify(restaurantRepository, times(1)).save(captor.capture());
+
+    Restaurant saveRestaurant = captor.getValue();
+
+    assertThat(restaurantSaveRequestDto.getName()).isEqualTo(saveRestaurant.getName());
+    assertThat(restaurantSaveRequestDto.getMinOrderPrice()).isEqualTo(saveRestaurant.getMinOrderPrice());
   }
 }
